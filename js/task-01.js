f83a86f00ceb0313f7cfee530b74bc9ab683ad8a
FelixPolkanov <felixpolkanov@gmail.com>
2022-09-26 16:16:44 +0300

    26/09

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const category = document.querySelector("#categories");
+const numberCategories = Category.querySelectorAll("li.item");
+console.log("Number of categories:", numberCategories.length);
+
+const ElementHeadingOfCategories = NumberCategories('h2');
+console.log(ElementHeadingOfCategories);
