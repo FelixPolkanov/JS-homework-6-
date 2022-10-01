75abcb4edbdfa5cbef92177659e216c07f530984
FelixPolkanov <felixpolkanov@gmail.com>
2022-10-01 18:03:37 +0300

    js-task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const formRef = document.querySelector(".login-form");
+
+formRef.addEventListener('submit', onFormSubmit);
+    
+
+    
+    function onFormSubmit (event) {
+    event.preventDefault();
+       
+
+        const formElements = event.currentTarget.elements
+        const email = formElements.email.value;
+        const password = formElements.password.value;
+
+        if (email === "" || password === "" ) {
+             return alert('Все поля должны быть заполнены!!!')
+          
+}
+
+        const data = {
+        email,
+        password,
+}
+        console.log(data)
+       event.currentTarget.reset();
+       
+    }
 
