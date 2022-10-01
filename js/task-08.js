9ff3968719fdeb864725ed7d2c60cc1153313968
FelixPolkanov <felixpolkanov@gmail.com>
2022-10-01 21:06:42 +0300

    js-task-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,28 +1,25 @@
+
 const formRef = document.querySelector(".login-form");
 
 formRef.addEventListener('submit', onFormSubmit);
-    
-
-    
-    function onFormSubmit (event) {
-    event.preventDefault();
+      
+function onFormSubmit (event) {
+event.preventDefault();
        
+const formElements = event.currentTarget.elements
+const email = formElements.email.value;
+const password = formElements.password.value;
 
-        const formElements = event.currentTarget.elements
-        const email = formElements.email.value;
-        const password = formElements.password.value;
-
-        if (email === "" || password === "" ) {
-             return alert('Все поля должны быть заполнены!!!')
-          
+if (email === "" || password === "" ) {
+return alert('Все поля должны быть заполнены!!!')
 }
 
-        const data = {
-        email,
-        password,
+const data = {
+email,
+password,
 }
-        console.log(data)
-       event.currentTarget.reset();
+console.log(data)
+event.currentTarget.reset();
        
-    }
+}
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonRef = document.querySelector('.change-color');
+
+buttonRef.addEventListener('click', eventBtnChangeColor);
+
+function eventBtnChangeColor(event) {
+  let bodyChangeColor = document.querySelector("body")
+  let bodyChangeColorFunction = getRandomHexColor();
+  
+  bodyChangeColor.style.backgroundColor = bodyChangeColorFunction;
+
+  const bodyColorName = document.querySelector('.color')
+  bodyColorName.textContent = bodyChangeColorFunction;
+
+}
+
